2fe2a13278a9fb6fa9ea5de158613b50aba1e182
Anatoliy-13 <arhangeltm13@gmail.com>
2023-04-17 14:36:06 +0200

    feat: add stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
+  push(element) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    Stack.push(element)
   }
 
   pop() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (Stack.length == 0)
+    return "Underflow";
+    return Stack.pop();
   }
 
   peek() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return Stack(Stack.length - 1)
   }
 }
 
